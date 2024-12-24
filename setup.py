b7213c7f94df0c0fbd1b2cd50bb8c6f6b82a240d
raunakgola <raunakgola9082@gmail.com>
2024-12-24 13:38:28 +0530

    changes

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Radial-Bias',
+    version='0.1.0',
+    description='A simple Python project library.',
+    author='Raunak',
+    author_email='raunakgola9082@gmail.com',
+    packages=find_packages(),
+    install_requires=[ "requests >=2.25.1",
+    "numpy >=1.20.0"],  # List your dependencies here
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
