7003a593d13e06414a3ffd39f4922ebb5df942aa
raunakgola <raunakgola9082@gmail.com>
2024-12-25 11:25:28 +0530

    some changes

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='Radial-Bias',
-    version='0.1.0',
+    version='0.1.1',
     description='A simple Python project library.',
     author='Raunak',
     author_email='raunakgola9082@gmail.com',
-    packages=find_packages(),
+    packages=find_packages(where="src"),
+    package_dir={"":"src"},
     install_requires=[ "requests >=2.25.1",
     "numpy >=1.20.0"],  # List your dependencies here
     classifiers=[
--- tests/test_module.py
+++ tests/test_module.py
@@ -1,11 +1,13 @@
 import unittest
-from src.Radial_bias_Raunak_gola import add
+# from src.Radial_bias_Raunak_gola import module
+from Radial_bias_Raunak_gola import module
+
 
 class TestAddFunction(unittest.TestCase):
     def test_add(self):
-        self.assertEqual(add(1, 2), 3)
-        self.assertEqual(add(-1, 1), 0)
-        self.assertEqual(add(-1, -1), -2)
+        self.assertEqual(module.add(1, 2), 3)
+        self.assertEqual(module.add(-1, 1), 0)
+        self.assertEqual(module.add(-1, -1), -2)
 
 if __name__ == '__main__':
     unittest.main()
