053940f63d63f5d8bc062bcd49a1cf568712c36c
raunakgola <raunakgola9082@gmail.com>
2024-12-25 17:54:13 +0530

    changes

--- tests/test_module.py
+++ tests/test_module.py
@@ -1,10 +1,6 @@
 import unittest
 from Radial_bias_Raunak_gola import module
 
-<<<<<<< HEAD
-=======
-
->>>>>>> b8367bba5a8bb4e5d72009b59d9b227e5e051c5d
 class TestAddFunction(unittest.TestCase):
     def test_add(self):
         self.assertEqual(module.add(1, 2), 3)
